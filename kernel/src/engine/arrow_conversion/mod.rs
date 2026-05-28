40438cd9967c8a0359767e1de9cbdf5af2807f23
Adam Reeve <adam.reeve@gr-oss.io>
2026-05-28 13:43:42 +1200

    Fallback to old behaviour when converting from arrow if feature disabled

--- kernel/src/engine/arrow_conversion/mod.rs
+++ kernel/src/engine/arrow_conversion/mod.rs
@@ -597,6 +597,16 @@ impl TryFromArrow<&ArrowDataType> for DataType {
             {
                 Ok(DataType::TIMESTAMP_NANOS)
             }
+            // Fall back to converting nanosecond timestamps to microsecond when the
+            // nanosecond-timestamp feature is not enabled.
+            #[cfg(not(feature = "nanosecond-timestamps"))]
+            ArrowDataType::Timestamp(TimeUnit::Nanosecond, None) => Ok(DataType::TIMESTAMP_NTZ),
+            #[cfg(not(feature = "nanosecond-timestamps"))]
+            ArrowDataType::Timestamp(TimeUnit::Nanosecond, Some(tz))
+                if tz.eq_ignore_ascii_case("utc") =>
+            {
+                Ok(DataType::TIMESTAMP)
+            }
             ArrowDataType::Struct(fields) => DataType::try_struct_type_from_results(
                 fields.iter().map(|field| field.as_ref().try_into_kernel()),
             )
